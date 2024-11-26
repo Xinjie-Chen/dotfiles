fedc0662dc9cd3e02be0f639ed3ad6175fbaa90c
Chen Xinjie <xinjiechen04@gmail.com>
2024-11-26 15:40:02 +0800

    update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,9 +6,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.b.autoformat = true
     vim.opt_local.expandtab = true
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
   end,
 })
 
